c9f13a685fd32f769921a670ea5ce42c4a51022a
Mauro Sabatino <maurosabatino@gmail.com>
2016-04-04 10:47:03 +0200

    first example

--- hello.ts
+++ hello.ts
@@ -1,2 +1,19 @@
-//welcome to learning space
-var ciao = "ciao Mauro";
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
